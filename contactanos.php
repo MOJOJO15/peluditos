5a547a64575c143605ba9334019963e11d0ff327
MOJOJO15 <quilumbarubi3@gmail.com>
2024-05-06 22:16:47 -0500

    Add files via upload

---
+++ contactanos.php
@@ -0,0 +1,61 @@
+<html>
+<?php 
+
+include('head.php');
+include('header.php');
+
+
+?>
+
+
+<body>
+
+
+
+<div class="container">
+<div class="row">
+
+<div class="col-md-6">
+<h2>Direccion:</h2>
+
+<p>Teléfono:0960316888</p>
+<p>E/mail:quilumbarubi3@gmail.com</p>
+</ul>
+<form method="post"action="envia.php">
+<div class="col-md-6">
+<input class="form-control" type="text"placeholder="Nombre" name="nombre"><br>
+<input class="form-control" type="text"placeholder="Apellido" name="apellido">
+
+
+</div>
+
+
+<div class="col-md-6">
+<input class="form-control" type="text"placeholder="Raza de su cachorro" name="Raza de su cachorro"><br>
+<input class="form-control" type="text"placeholder="Teléfono" name="Telefono">
+</div>
+
+</form>
+
+
+</div>
+
+<div class="col-md-6">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7961016198237!2d-78.3833485!3d-0.19121449999999404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d593ddf3bb702d%3A0x962af2736bcfe2aa!2sLula%20peluquerias!5e0!3m2!1ses-419!2sec!4v1714969909854!5m2!1ses-419!2sec" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+</div>
+
+
+
+
+</div>
+</div>
+
+</body>
+<?php include('footer.php');?>
+
+
+
+
+
+</html>
